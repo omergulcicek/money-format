4bcf008a364d1a4ff7978bed8aeba979cb3320bf
Ömer Gülçiçek <iletisim@omergulcicek.com>
2021-04-25 13:07:26 +0300

    Update moneyFormat.js

--- moneyFormat.js
+++ moneyFormat.js
@@ -1,22 +1,28 @@
+/*!
+ * Ömer Gülçiçek
+ * https://omergulcicek.com
+ * https://github.com/omergulcicek/money-format
+ */
+
 function moneyFormat(n) {
-    if (n == null || n === "" || typeof n == "object") { return "" }
-    else if (typeof n == "number") { n = n.toString() }
+  if (n == null || n === "" || typeof n == "object") { return "" }
+  else if (typeof n == "number") { n = n.toString() }
 
-    var a, x, reverseTemp = "", reverse = "";
+  var a, x, reverseTemp = "", reverse = "";
 
-    a = parseFloat(n.replace(",", ".")).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1.');
+  a = parseFloat(n.replace(",", ".")).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1.');
 
-    x = a.substr(a.length - 3).replace(".", ",");
+  x = a.substr(a.length - 3).replace(".", ",");
 
-    for (i = a.length - 1; i >= 0; i--) { reverseTemp += a[i]; }
+  for (i = a.length - 1; i >= 0; i--) { reverseTemp += a[i]; }
 
-    reverseTemp = reverseTemp.replace(".", ",");
+  reverseTemp = reverseTemp.replace(".", ",");
 
-    for (i = reverseTemp.length - 1; i >= 0; i--) { reverse += reverseTemp[i]; }
+  for (i = reverseTemp.length - 1; i >= 0; i--) { reverse += reverseTemp[i]; }
 
-    if (x == ",00") {
-        reverse = reverse.slice(0, a.length-3)
-    }
-    
-    return reverse;
+  if (x == ",00") {
+      reverse = reverse.slice(0, a.length-3)
+  }
+
+  return reverse;
 }
